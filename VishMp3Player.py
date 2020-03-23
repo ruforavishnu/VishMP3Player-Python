dc1aa299f84f2d188a319e84225d0456dbbb5526
Vishnu <vishnuajith310@gmail.com>
2020-03-23 22:10:47 +0530

    added volume increase/decrease functionality

--- VishMp3Player.py
+++ VishMp3Player.py
@@ -11,9 +11,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 import pygame
 
 
-isMusicPlaying = "False"
-
-
 
 
 class Ui_MainWindow(object):
@@ -31,38 +28,45 @@ class Ui_MainWindow(object):
         self.playButton.setObjectName("playButton")
         self.playButton.clicked.connect(self.playButton_onClicked)
 
+
         self.pauseButton = QtWidgets.QPushButton(self.centralwidget)
         self.pauseButton.setGeometry(QtCore.QRect(20, 60, 93, 28))
         self.pauseButton.setObjectName("pauseButton")
         self.pauseButton.clicked.connect(self.pauseButton_onClicked)
 
 
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(20, 110, 93, 28))
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(20, 160, 93, 28))
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setGeometry(QtCore.QRect(20, 210, 93, 28))
-        self.pushButton_5.setObjectName("pushButton_5")
-        self.listView = QtWidgets.QListView(self.centralwidget)
-        self.listView.setGeometry(QtCore.QRect(20, 340, 256, 261))
-        self.listView.setObjectName("listView")
-        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_6.setGeometry(QtCore.QRect(30, 620, 93, 28))
-        self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_7.setGeometry(QtCore.QRect(160, 620, 93, 28))
-        self.pushButton_7.setObjectName("pushButton_7")
-        self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
-        self.horizontalSlider.setGeometry(QtCore.QRect(30, 290, 231, 22))
-        self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
-        self.horizontalSlider.setObjectName("horizontalSlider")
-        self.verticalSlider = QtWidgets.QSlider(self.centralwidget)
-        self.verticalSlider.setGeometry(QtCore.QRect(250, 20, 22, 91))
-        self.verticalSlider.setOrientation(QtCore.Qt.Vertical)
-        self.verticalSlider.setObjectName("verticalSlider")
+        self.stopButton = QtWidgets.QPushButton(self.centralwidget)
+        self.stopButton.setGeometry(QtCore.QRect(20, 110, 93, 28))
+        self.stopButton.setObjectName("stopButton")
+        self.stopButton.clicked.connect(self.stopButton_onClicked)
+
+        self.nextTrackButton = QtWidgets.QPushButton(self.centralwidget)
+        self.nextTrackButton.setGeometry(QtCore.QRect(20, 160, 93, 28))
+        self.nextTrackButton.setObjectName("nextTrackButton")
+        self.prevTrackButton = QtWidgets.QPushButton(self.centralwidget)
+        self.prevTrackButton.setGeometry(QtCore.QRect(20, 210, 93, 28))
+        self.prevTrackButton.setObjectName("prevTrackButton")
+        self.playlistListView = QtWidgets.QListView(self.centralwidget)
+        self.playlistListView.setGeometry(QtCore.QRect(20, 340, 256, 261))
+        self.playlistListView.setObjectName("playlistListView")
+        self.addMP3ToPlaylistButton = QtWidgets.QPushButton(self.centralwidget)
+        self.addMP3ToPlaylistButton.setGeometry(QtCore.QRect(30, 620, 93, 28))
+        self.addMP3ToPlaylistButton.setObjectName("addMP3ToPlaylistButton")
+        self.removeMP3FromPlaylistButton = QtWidgets.QPushButton(self.centralwidget)
+        self.removeMP3FromPlaylistButton.setGeometry(QtCore.QRect(160, 620, 93, 28))
+        self.removeMP3FromPlaylistButton.setObjectName("removeMP3FromPlaylistButton")
+        self.timeSeekHorizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.timeSeekHorizontalSlider.setGeometry(QtCore.QRect(30, 290, 231, 22))
+        self.timeSeekHorizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.timeSeekHorizontalSlider.setObjectName("timeSeekHorizontalSlider")
+  #      self.timeSeekHorizontalSlider.valueChanged.connect(self.timeSeekHorizontalSlider_onValueChanged)
+  
+        self.volumeVerticalSlider = QtWidgets.QSlider(self.centralwidget)
+        self.volumeVerticalSlider.setGeometry(QtCore.QRect(250, 20, 22, 91))
+        self.volumeVerticalSlider.setOrientation(QtCore.Qt.Vertical)
+        self.volumeVerticalSlider.setObjectName("volumeVerticalSlider")
+        self.volumeVerticalSlider.valueChanged.connect(self.volumeVerticalSlider_onValueChanged)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 300, 26))
@@ -75,31 +79,36 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "VishMP3 Player"))
         self.playButton.setText(_translate("MainWindow", "Play"))
         self.pauseButton.setText(_translate("MainWindow", "Pause"))
-        self.pushButton_3.setText(_translate("MainWindow", "Stop"))
-        self.pushButton_4.setText(_translate("MainWindow", "Next Track"))
-        self.pushButton_5.setText(_translate("MainWindow", "Prev Track"))
-        self.pushButton_6.setText(_translate("MainWindow", "Add Mp3"))
-        self.pushButton_7.setText(_translate("MainWindow", "Remove Mp3"))
+        self.stopButton.setText(_translate("MainWindow", "Stop"))
+        self.nextTrackButton.setText(_translate("MainWindow", "Next Track"))
+        self.prevTrackButton.setText(_translate("MainWindow", "Prev Track"))
+        self.addMP3ToPlaylistButton.setText(_translate("MainWindow", "Add Mp3"))
+        self.removeMP3FromPlaylistButton.setText(_translate("MainWindow", "Remove Mp3"))
+
+    def volumeVerticalSlider_onValueChanged(self):
+        offsetValue = self.volumeVerticalSlider.value()
+        print("volume offset value:"+str(offsetValue))
+        changeCurrentVolumeToValue(offsetValue)
+
+
 
     def playButton_onClicked(self):
-        print("play button clicked")
-        
-        try:
-            initMixer()
-            filename = 'D:\\bumbro.mp3'
-            playmusic(filename)
-            self.isMusicPlaying = "True"
-        except KeyboardInterrupt:   # to stop playing, press "ctrl-c"
-            print("\nPlay Stopped by user")
-        except Exception:
-            print("unknown error")
+            print("play button clicked")
+            
+            try:
+                initMixer()
+                filename = 'D:\\bumbro.mp3'
+                playmusic(filename)
+                self.isMusicPlaying = "True"
+            except KeyboardInterrupt:   # to stop playing, press "ctrl-c"
+                print("\nPlay Stopped by user")
+            except Exception:
+                print("unknown error")
 
     def pauseButton_onClicked(self):
         print("pause button clicked")
@@ -127,7 +136,14 @@ class Ui_MainWindow(object):
             print("Exception occured:"+e)
 
 
-#############################################################
+    def stopButton_onClicked(self):
+        print("stop button clicked")
+        try:
+            stopmusic()
+        except Exception as e:
+            print("Exception occured while trying to invoke stopmusic() function")
+
+    #############################################################
 
 
 
@@ -147,7 +163,10 @@ def playsound(soundfile):
 def pauseMusic():
     pygame.mixer.music.pause()
     print("pygame pauseMusic function invoked")
-    
+#    currentTime = pygame.mixer.music.get_pos()
+ #   print("current time of song:"+str(currentTime))
+
+
 def unPauseMusic():
     pygame.mixer.music.unpause()
     print("pygame unPauseMusic function invoked")
@@ -173,6 +192,11 @@ def stopmusic():
     """stop currently playing music"""
     pygame.mixer.music.stop()
 
+def changeCurrentVolumeToValue(reqdVolumeValue):
+    volumeValue = reqdVolumeValue/100.0
+    print("volume value:"+str(volumeValue))
+    pygame.mixer.music.set_volume(volumeValue)
+
 def getmixerargs():
     pygame.mixer.init()
     freq, size, chan = pygame.mixer.get_init()
@@ -195,10 +219,7 @@ if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
-
-
